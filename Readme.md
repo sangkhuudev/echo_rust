f05620ebe70664336b539b68311561602c44b201
Khuu Viet Sang <sangkhuudev@gmail.com>
2023-07-06 12:02:17 +0700

    Full version of Echo

---
+++ Readme.md
@@ -0,0 +1,3 @@
+This is  a program to run Echo command line written in Rust.
+
+It consists of ingtegration test to cover some cases to be an exact version of "Echo command in Linux"
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use clap::{App,Arg};
+
+fn main() {
+    let matches = App::new("echor")
+        .version("0.1.0")
+        .author("sangkhuudev")
+        .about("Rust echo")
+        .arg(
+            Arg::with_name("text")
+            .value_name("Text")
+            .help("Input text")
+            .required(true)
+            .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+            .short("n")
+            .help("Do not print new line")
+            .takes_value(false),
+        )
+        .get_matches();
+    // let text= matches.values_of_lossy("text").unwrap();
+    // let omit_newline = matches.is_present("omit_newline");
+    // let mut ending = "\n";
+    // if omit_newline {
+    //     ending = "";
+    // }
+    // println!("{}{}", text.join(" "), ending);
+
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline = matches.is_present("omit_newline");
+    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
+}
